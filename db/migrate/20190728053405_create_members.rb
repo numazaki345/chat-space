d5d6df09e603275d5a8e1e305ae5a808b53f9a33
ぬー <ryo.091979@gmail.com>
2019-08-22 18:11:21 +0900

    Revert "a"
    This reverts commit b5c7f90e59cb88d991a35b59cad61cbc0f76830e.

--- db/migrate/20190728053405_create_members.rb
+++
@@ -1,9 +0,0 @@
-class CreateMembers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :members do |t|
-      t.integer :user_id, foreign_key: true
-      t.integer :group_id, foreign_key: true
-      t.timestamps
-    end
-  end
-end
